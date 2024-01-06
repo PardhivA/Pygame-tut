55bc3730833d5542dfa5fefe0a966a7bf066c066
PardhivA <pardhiv19nov@gmail.com>
2024-01-06 23:29:14 +0530

    Some Changes

--- p1.py
+++ p1.py
@@ -9,7 +9,7 @@ test_font=  pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('./graphics/Sky.png').convert()
 ground_surface = pygame.image.load('./graphics/ground.png').convert()
-text_surface = test_font.render('My Game', False ,'Black').convert()
+text_surface = test_font.render('AIm to WIN', False ,'Black').convert()
 
 snail_surface = pygame.image.load('./graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
@@ -21,7 +21,7 @@ while True:
             exit()
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(sky_surface,(300,50))
+    screen.blit(text_surface,(325,50))
     snail_x_pos -= 4
     if(snail_x_pos < -100): snail_x_pos = 600
     screen.blit(snail_surface,(snail_x_pos,270))
